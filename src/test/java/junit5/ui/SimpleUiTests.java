e6ffe6da9ef4f1d69d38a868f0e0714525da0c9a
Oleg <oleole120891@gmail.com>
2024-08-30 10:33:52 +0800

    commit 2

--- src/test/java/junit5/ui/SimpleUiTests.java
+++ src/test/java/junit5/ui/SimpleUiTests.java
@@ -19,6 +19,7 @@ public class SimpleUiTests {
     private WebDriver driver;
     private String downloadFolder = System.getProperty("User.dir") + File.separator + "build" + File.separator + "downloadFiles";
     private String k;
+    private String a;
 
     @BeforeAll
     public static void downloadDriver() {
